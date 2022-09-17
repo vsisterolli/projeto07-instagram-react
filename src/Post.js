8e7967a483895c2ce241a4a318b447bc811a70b0
vsisterolli <vsisterolli@gmail.com>
2022-09-16 22:29:33 -0300

    Finished interactions

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,47 @@
-import { useReducer } from "react";
+import React from 'react';
 
 export default function Post(props) {
+    
+    const [curtidas, setCurtidas] = React.useState(props.curtidas);
+    const [curtido, setCurtido] = React.useState(false);
+    
+    const [classeCurtir, setClasse] = React.useState("");
+    const [iconeCurtir, setIconeCurtir] = React.useState("heart-outline");
+
+    const [iconeSalvo, setIconeSalvo] = React.useState("bookmark-outline");
+    const [classeSalvo, setClasseSalvo] = React.useState("");
+
+    function toggleCurtidas(foiDaImagem) {
+
+        console.log(foiDaImagem);
+        if(foiDaImagem && curtido)
+            return;
+        
+        let aux = String(curtidas);
+        aux = aux.replaceAll('.', '')
+
+        aux = Number(aux);
+        aux += (curtido ? -1 : 1);
+
+        setClasse(curtido ? "" : "curtido");
+        setIconeCurtir(curtido ? "heart-outline" : "heart");
+
+        setCurtido(!curtido);
+        setCurtidas(aux.toLocaleString('pt-BR'));
+        
+    }
+
+    function toggleSalvo() {
+        if(iconeSalvo === "bookmark-outline") {
+            setIconeSalvo("bookmark");
+            setClasseSalvo("salvo");
+        }
+        else {
+            setIconeSalvo("bookmark-outline");
+            setClasseSalvo("");
+        }
+    }
+
     return(
     <div class="post">
         <div class="topo">
@@ -14,25 +55,25 @@ export default function Post(props) {
         </div>
 
         <div class="conteudo">
-            <img alt="texto" src={props.postImg} />
+            <img alt="texto" src={props.postImg} onClick={() => toggleCurtidas(true)}/>
         </div>
 
         <div class="fundo">
             <div class="acoes">
                 <div>
-                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon class={classeCurtir} name={iconeCurtir} onClick={() => toggleCurtidas(false)}></ion-icon>
                     <ion-icon name="chatbubble-outline"></ion-icon>
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                    <ion-icon name={iconeSalvo} class={classeSalvo} onClick={toggleSalvo}></ion-icon>
                 </div>
             </div>
 
             <div class="curtidas">
-                <img alt="texto" src="assets/img/respondeai.svg" />
+                <img alt="texto" src="assets/img/respondeai.svg"/>
                 <div class="texto">
-                    Curtido por <strong>{props.destaque}</strong> e <strong>outras {props.curtidas} pessoas</strong>
+                    Curtido por <strong>{props.destaque}</strong> e <strong>outras {curtidas} pessoas</strong>
                 </div>
             </div>
             
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,11 +1,17 @@
+import React from 'react';
+
 export default function Usuario() {
+    
+    const [nome, setNome] = React.useState("Catana");
+    const [foto, setFoto] = React.useState("assets/img/catanacomics.svg")
+
     return(
     <div class="usuario">
-        <img alt="texto" src="assets/img/catanacomics.svg" />
+        <img alt="texto" src={foto} onClick={() => setFoto(prompt("Qual o link da sua foto?"))}/>
         <div class="texto">
         <strong>catanacomics</strong>
-        <span>
-             Catana
+        <span onClick={() => setNome(prompt("Qual seu nome?"))}>
+            {nome}
             <ion-icon name="pencil"></ion-icon>
         </span>
         </div>
